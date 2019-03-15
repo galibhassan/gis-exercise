88a50b4f7417d8c0600db1072ff2ac5f33f4e5ca
Galib Hassan <Galib.Hassan@autodesk.com>
2019-03-15 04:36:26 +0100

    with unsatisfactory plots for probB

--- problemB.R
+++ problemB.R
@@ -41,7 +41,7 @@ source('C:/work/learning/gettingSmarter/personal/tanz-rem-sens-hw/problemA.R')
 # make an array of incident angles from 0 to 80 degree
 theta1_degree = seq(from=0, to=80, by=.1)
 
-# E_r = 3
+# E_r = 3 --------------------------------------
 R_s_3 = getRs(
   3, 
   theta1_degree, 
@@ -57,6 +57,38 @@ R_p_3 = getRp(
 plot(x = theta1_degree, y = R_s_3, col='darkblue')
 points(x = theta1_degree, y = R_p_3, col='red')
 
+# E_r = 10 --------------------------------------
+R_s_10 = getRs(
+  10, 
+  theta1_degree,
+  getTransmissionAngle(10, theta1_degree)
+)
+
+R_p_10 = getRp(
+  10,
+  theta1_degree,
+  getTransmissionAngle(10, theta1_degree)
+)
+
+points(x = theta1_degree, y = R_s_10, col='green')
+points(x = theta1_degree, y = R_p_10, col='blue')
+
+# E_r = 50 --------------------------------------
+
+R_s_50 = getRs(
+  50, 
+  theta1_degree,
+  getTransmissionAngle(50, theta1_degree)
+)
+
+R_p_50 = getRp(
+  50,
+  theta1_degree,
+  getTransmissionAngle(50, theta1_degree)
+)
+
+points(x = theta1_degree, y = R_s_50, col='orange')
+points(x = theta1_degree, y = R_p_50, col='magenta')
 
 
 
